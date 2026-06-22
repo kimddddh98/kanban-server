a16974694bf5f765a3328fac1bb00dd008d94132
kimddddh98 <amawang11@gmail.com>
2026-06-22 22:50:40 +0900

    Add ALB health check endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+type HealthCheckResponse = {
+  status: 'ok';
+};
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -8,5 +12,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('health')
+  getHealth(): HealthCheckResponse {
+    return { status: 'ok' };
   }
 }
